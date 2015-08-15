2950519daa8943d10f6beaa27f7b7862d8431c2b
uuu1993 <634468460@qq.com>
2015-08-15 09:30:27 +0800

    create OboTag.java

---
+++ MyApplication/app/src/main/java/com/obo/demo/obtag/OboTag.java
@@ -0,0 +1,38 @@
+package com.obo.demo.obtag;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by obo on 15/8/15.
+ */
+public class OboTag extends View {
+
+    public static String TAG = OboTag.class.getCanonicalName();
+
+    private Context context;
+
+    public OboTag(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        this.context = context;
+    }
+
+
+    @Override
+    public void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
+
+        canvas.drawCircle(100 , 100,50,new Paint());
+
+
+
+    }
+
+
+
+}
